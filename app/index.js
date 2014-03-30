ddddab555686ac98a0eb6c9ec681c0de8539f265
Martin Hecher <martin.hecher@fraunhofer.at>
2014-03-30 13:26:34 +0200

    working 'fetch' subgenerator

--- app/index.js
+++ app/index.js
@@ -6,50 +6,21 @@ var chalk = require('chalk');
 
 
 var PuppetmasterGenerator = yeoman.generators.Base.extend({
-  init: function () {
+  init: function() {
     this.pkg = require('../package.json');
 
-    this.on('end', function () {
+    this.on('end', function() {
       if (!this.options['skip-install']) {
         this.installDependencies();
       }
     });
   },
 
-  askFor: function () {
-    var done = this.async();
-
-    // have Yeoman greet the user
+  greet: function() {
     this.log(this.yeoman);
+    this.log(chalk.magenta("Thank's for using the fantastic Puppetmaster generator. Have fun i uživaj!"));
 
-    // replace it with a short and sweet description of your generator
-    this.log(chalk.magenta('You\'re using the fantastic Puppetmaster generator.'));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
-  },
-
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
-
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
-
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
+    // TODO: ask for dependent projects and write them to package.json
   }
 });
 
--- fetch/index.js
+++ fetch/index.js
@@ -1,27 +1,56 @@
 'use strict';
 var util = require('util');
+var path = require('path');
+var chalk = require('chalk');
 var yeoman = require('yeoman-generator');
+
 
 var FetchGenerator = yeoman.generators.NamedBase.extend({
 	init: function() {
-		console.log('You called the fetch subgenerator with the argument ' + this.name + '.');
+		if (typeof this.name === 'undefined') {
+			console.log('You have not specified a configuration, proceeding with "all"');
+			this.name = 'all';
+		}
+
+		// TODO: error handling
+		this.pkg = require(path.join(this.destinationRoot(), 'package.json'));
+
+		this.log(chalk.magenta('Jednu "konnector:' + this.name + '", molim. I tri kugle sladoleda!'));
 	},
 
-	files: function() {
-		var spawn = require('child_process').spawn,
-			ls = spawn('git', ['clone', 'git@github.com:martinhecher/' + this.name + '.git', '../' + this.name]);
+	fetchRepos: function() {
+		var repos = this.pkg.projects.repos,
+			path = this.pkg.projects.path;
 
-		ls.stdout.on('data', function(data) {
-			console.log('stdout: ' + data);
-		});
+		// TODO: error handling
+		this.mkdir(path);
 
-		ls.stderr.on('data', function(data) {
-			console.log('stderr: ' + data);
-		});
+		function onGitClose(reponame, code) {
+			if (!code) {
+				this.log(chalk.green('Fetched "' + reponame + '".'));
+			} else {
+				this.log(chalk.red('Something went wrong while fetching "' + reponame + '".'));
+			}
+		}
 
-		ls.on('close', function(code) {
-			console.log('child process exited with code ' + code);
-		});
+		for (var idx = 0; idx < repos.length; idx++) {
+			// TODO: support for other providers (factory/registry?)
+			var repo_name = repos[idx],
+				gitrepo = 'git@gitlab.com:konnektor/' + repo_name + '.git',
+				spawn = require('child_process').spawn,
+				git = spawn('git', ['clone', gitrepo, path + '/' + repos[idx]]);
+
+			// Yeah, thanks http://passy.svbtle.com/partial-application-in-javascript-using-bind ;-)
+			git.on('close', onGitClose.bind(this, repo_name));
+
+			// git.stdout.on('data', function(data) {
+			// 	this.log(chalk.green('Fetched "' + repo_name + '".'));
+			// }.bind(this));
+
+			// git.stderr.on('data', function(data) {
+			// 	this.log(chalk.red('Something went wrong while fetching "' + repo_name + '".'));
+			// }.bind(this));
+		}
 	}
 });
 
